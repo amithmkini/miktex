38e4bafba2106e398f1e0839a31054d5031d2bcf
Christian Schenk <cs@miktex.org>
2021-10-09 19:48:34 +0200

    pTeX

--- Programs/MiKTeX/initexmf/initexmf.cpp
+++ Programs/MiKTeX/initexmf/initexmf.cpp
@@ -1470,6 +1470,7 @@ vector<FileLink> miktexFileLinks =
   { MIKTEX_PSRESIZE_EXE, { "psresize" } },
   { MIKTEX_PSSELECT_EXE, { "psselect" } },
   { MIKTEX_PSTOPS_EXE, { "pstops" } },
+  { MIKTEX_PTFTOPL_EXE, { "ptftopl" } },
   { MIKTEX_REBAR_EXE, { "rebar" } },
   { MIKTEX_SCOR2PRT_EXE, { "scor2prt" } },
   { MIKTEX_SJISCONV_EXE, { "sjisconv" } },
