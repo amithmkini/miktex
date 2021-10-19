0b83cebf4580ff99acc0febc11ae6b3ff68006f2
Christian Schenk <christian.schenk@mitel.com>
2021-10-19 15:18:03 +0200

    ptex

--- Programs/TeXAndFriends/texjp/uptex/source/uptexextra.c
+++ Programs/TeXAndFriends/texjp/uptex/source/uptexextra.c
@@ -2,8 +2,13 @@
 
    This file is public domain.  */
 
+#if defined(MIKTEX)
+#include "miktex-uptex.h"
+using namespace MiKTeX::TeXAndFriends;
+#else
 #define	EXTERN /* Instantiate data from uptexd.h here.  */
 #define DLLPROC dlluptexmain
+#endif
 
 /* This file defines TeX and upTeX.  */
 #include <uptexd.h>
